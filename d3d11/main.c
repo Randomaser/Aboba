ec8969029dc55e1800a152b8373a09d3f1172f9b
Rad_Inc <igor.dymchenko5@gmail.com>
2022-06-05 22:58:10 +0300

    A

---
+++ d3d11/main.c
@@ -0,0 +1,395 @@
+/* nuklear - 1.32.0 - public domain */
+#define COBJMACROS
+#define WIN32_LEAN_AND_MEAN
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <d3d11.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_INDEX_BUFFER 128 * 1024
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_D3D11_IMPLEMENTATION
+//#define INCLUDE_OVERVIEW
+//#define INCLUDE_NODE_EDITOR
+#include "../../nuklear.h"
+//#include "nuklear_d3d11_pixel_shader.h"
+//#include "nuklear_d3d11_vertex_shader.h"
+#include "nuklear_d3d11.h"
+//#pragma comment(lib, "d3d11.lib")
+
+/* ===============================================================
+ *
+ *                          EXAMPLE
+ *
+ * ===============================================================*/
+/* This are some code examples to provide a small overview of what can be
+ * done with this library. To try out an example uncomment the defines */
+/*#define INCLUDE_ALL */
+/*#define INCLUDE_STYLE */
+/*#define INCLUDE_CALCULATOR */
+/*#define INCLUDE_CANVAS */
+/*#define INCLUDE_OVERVIEW */
+/*#define INCLUDE_NODE_EDITOR */
+
+#ifdef INCLUDE_ALL
+  #define INCLUDE_STYLE
+  #define INCLUDE_CALCULATOR
+  #define INCLUDE_CANVAS
+  #define INCLUDE_OVERVIEW
+  #define INCLUDE_NODE_EDITOR
+#endif
+
+#ifdef INCLUDE_STYLE
+  #include "../../demo/common/style.c"
+#endif
+#ifdef INCLUDE_CALCULATOR
+  #include "../../demo/common/calculator.c"
+#endif
+#ifdef INCLUDE_CANVAS
+  #include "../../demo/common/canvas.c"
+#endif
+#ifdef INCLUDE_OVERVIEW
+  #include "../../demo/common/overview.c"
+#endif
+#ifdef INCLUDE_NODE_EDITOR
+  #include "../../demo/common/node_editor.c"
+#endif
+
+/* ===============================================================
+ *
+ *                          DEMO
+ *
+ * ===============================================================*/
+static IDXGISwapChain *swap_chain;
+static ID3D11Device *device;
+static ID3D11DeviceContext *context;
+static ID3D11RenderTargetView* rt_view;
+
+typedef struct _record
+{
+    int PK;
+    char name[30];
+    float pay;
+    int count;
+} rec;
+
+rec table[30];
+int rec_count;
+char buffer[256];
+char result[30][3][30];
+char res_out = 0;
+
+void clear_buf(char* buf, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        buf[i] = '\0';
+    }
+}
+
+static void
+set_swap_chain_size(int width, int height)
+{
+    ID3D11Texture2D *back_buffer;
+    D3D11_RENDER_TARGET_VIEW_DESC desc;
+    HRESULT hr;
+
+    if (rt_view)
+        ID3D11RenderTargetView_Release(rt_view);
+
+    ID3D11DeviceContext_OMSetRenderTargets(context, 0, NULL, NULL);
+
+    hr = IDXGISwapChain_ResizeBuffers(swap_chain, 0, width, height, DXGI_FORMAT_UNKNOWN, 0);
+    if (hr == DXGI_ERROR_DEVICE_REMOVED || hr == DXGI_ERROR_DEVICE_RESET || hr == DXGI_ERROR_DRIVER_INTERNAL_ERROR)
+    {
+        /* to recover from this, you'll need to recreate device and all the resources */
+        MessageBoxW(NULL, L"DXGI device is removed or reset!", L"Error", 0);
+        exit(0);
+    }
+    assert(SUCCEEDED(hr));
+
+    memset(&desc, 0, sizeof(desc));
+    desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    desc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+
+    hr = IDXGISwapChain_GetBuffer(swap_chain, 0, &IID_ID3D11Texture2D, (void **)&back_buffer);
+    assert(SUCCEEDED(hr));
+
+    hr = ID3D11Device_CreateRenderTargetView(device, (ID3D11Resource *)back_buffer, &desc, &rt_view);
+    assert(SUCCEEDED(hr));
+
+    ID3D11Texture2D_Release(back_buffer);
+}
+
+static LRESULT CALLBACK
+WindowProc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_SIZE:
+        if (swap_chain)
+        {
+            int width = LOWORD(lparam);
+            int height = HIWORD(lparam);
+            set_swap_chain_size(width, height);
+            nk_d3d11_resize(context, width, height);
+        }
+        break;
+    }
+
+    if (nk_d3d11_handle_event(wnd, msg, wparam, lparam))
+        return 0;
+
+    return DefWindowProcW(wnd, msg, wparam, lparam);
+}
+
+int main(void)
+{
+    struct nk_context *ctx;
+    struct nk_colorf bg;
+
+    WNDCLASSW wc;
+    RECT rect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+    DWORD style = WS_OVERLAPPEDWINDOW;
+    DWORD exstyle = WS_EX_APPWINDOW;
+    HWND wnd;
+    int running = 1;
+    HRESULT hr;
+    D3D_FEATURE_LEVEL feature_level;
+    DXGI_SWAP_CHAIN_DESC swap_chain_desc;
+
+    /* Win32 */
+    memset(&wc, 0, sizeof(wc));
+    wc.style = CS_DBLCLKS;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandleW(0);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = L"NuklearWindowClass";
+    RegisterClassW(&wc);
+
+    AdjustWindowRectEx(&rect, style, FALSE, exstyle);
+
+    wnd = CreateWindowExW(exstyle, wc.lpszClassName, L"Aboba",
+        style | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
+        rect.right - rect.left, rect.bottom - rect.top,
+        NULL, NULL, wc.hInstance, NULL);
+
+    /* D3D11 setup */
+    memset(&swap_chain_desc, 0, sizeof(swap_chain_desc));
+    swap_chain_desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swap_chain_desc.BufferDesc.RefreshRate.Numerator = 60;
+    swap_chain_desc.BufferDesc.RefreshRate.Denominator = 1;
+    swap_chain_desc.SampleDesc.Count = 1;
+    swap_chain_desc.SampleDesc.Quality = 0;
+    swap_chain_desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swap_chain_desc.BufferCount = 1;
+    swap_chain_desc.OutputWindow = wnd;
+    swap_chain_desc.Windowed = TRUE;
+    swap_chain_desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+    swap_chain_desc.Flags = 0;
+    if (FAILED(D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE,
+        NULL, 0, NULL, 0, D3D11_SDK_VERSION, &swap_chain_desc,
+        &swap_chain, &device, &feature_level, &context)))
+    {
+        /* if hardware device fails, then try WARP high-performance
+           software rasterizer, this is useful for RDP sessions */
+        hr = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_WARP,
+            NULL, 0, NULL, 0, D3D11_SDK_VERSION, &swap_chain_desc,
+            &swap_chain, &device, &feature_level, &context);
+        assert(SUCCEEDED(hr));
+    }
+    set_swap_chain_size(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    strcpy(buffer, "D:\\Database.txt");
+
+    /* GUI */
+    ctx = nk_d3d11_init(device, WINDOW_WIDTH, WINDOW_HEIGHT, MAX_VERTEX_BUFFER, MAX_INDEX_BUFFER);
+    /* Load Fonts: if none of these are loaded a default font will be used  */
+    /* Load Cursor: if you uncomment cursor loading please hide the cursor */
+    {struct nk_font_atlas *atlas;
+    nk_d3d11_font_stash_begin(&atlas);
+    /*struct nk_font *droid = nk_font_atlas_add_from_file(atlas, "../../extra_font/DroidSans.ttf", 14, 0);*/
+    /*struct nk_font *robot = nk_font_atlas_add_from_file(atlas, "../../extra_font/Roboto-Regular.ttf", 14, 0);*/
+    /*struct nk_font *future = nk_font_atlas_add_from_file(atlas, "../../extra_font/kenvector_future_thin.ttf", 13, 0);*/
+    /*struct nk_font *clean = nk_font_atlas_add_from_file(atlas, "../../extra_font/ProggyClean.ttf", 12, 0);*/
+    /*struct nk_font *tiny = nk_font_atlas_add_from_file(atlas, "../../extra_font/ProggyTiny.ttf", 10, 0);*/
+    /*struct nk_font *cousine = nk_font_atlas_add_from_file(atlas, "../../extra_font/Cousine-Regular.ttf", 13, 0);*/
+    nk_d3d11_font_stash_end();
+    /*nk_style_load_all_cursors(ctx, atlas->cursors);*/
+    /*nk_style_set_font(ctx, &droid->handle)*/;}
+
+    /* style.c */
+    #ifdef INCLUDE_STYLE
+    /*set_style(ctx, THEME_WHITE);*/
+    /*set_style(ctx, THEME_RED);*/
+    /*set_style(ctx, THEME_BLUE);*/
+    /*set_style(ctx, THEME_DARK);*/
+    #endif
+
+    bg.r = 0.10f, bg.g = 0.18f, bg.b = 0.24f, bg.a = 1.0f;
+    while (running)
+    {
+        /* Input */
+        MSG msg;
+        nk_input_begin(ctx);
+        while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                running = 0;
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+        nk_input_end(ctx);
+
+        /* GUI */
+        if (nk_begin(ctx, "Calculator", nk_rect(50, 50, 480, 400),
+            NK_WINDOW_BORDER | NK_WINDOW_MOVABLE /*| NK_WINDOW_SCALABLE*/ |
+            NK_WINDOW_MINIMIZABLE | NK_WINDOW_TITLE))
+        {
+            nk_layout_row_dynamic(ctx, 30, 1);
+            nk_label_wrap(ctx, "Hello! This program can calculate cost of one call telephone company!", NK_TEXT_CENTERED);
+            nk_layout_row_dynamic(ctx, 60, 1);
+            nk_label(ctx, "Please, write the path to the database file below:", NK_TEXT_ALIGN_BOTTOM | NK_TEXT_ALIGN_LEFT);
+            nk_layout_row_dynamic(ctx, 40, 1);
+            nk_flags event = nk_edit_string_zero_terminated(ctx,
+                NK_EDIT_BOX | NK_EDIT_AUTO_SELECT, //fcous will auto select all text (NK_EDIT_BOX not sure)
+                buffer, sizeof(buffer), nk_filter_ascii);
+
+            for (int i = 0; i < rec_count; i++)
+            {
+                nk_layout_row_dynamic(ctx, 15, 2);
+                nk_label(ctx, result[i][0], NK_TEXT_CENTERED);
+                nk_label(ctx, result[i][1], NK_TEXT_CENTERED);
+            }
+                    
+            nk_layout_row_dynamic(ctx, 60, 1);
+            nk_layout_row_dynamic(ctx, 30, 1);
+            if (nk_button_label(ctx, "Calculate!"))
+            {
+                rec_count = 0;
+                res_out = 0;
+                char boo = 0;
+                if (!boo)
+                {
+                    char* filename = buffer;//"D:\\Database.txt";
+                    FILE* fp;
+                    if ((fp = fopen(filename, "r+")) != NULL)
+                    {
+                        enum cols {
+                            NAME,
+                            PAY,
+                            COUNT
+                        };
+                        char c;
+                        char buf[30];
+                        int j = 0;
+                        int i = 0;
+                        clear_buf(buf, 30);
+
+                        while ((c = getc(fp)) != EOF)
+                        {
+                            if (c == '\t' || (c == '\n' && i == 2))
+                            {
+                                switch (i)
+                                {
+                                case NAME:
+                                    table[rec_count].PK = rec_count;
+                                    for (int ji = 0; ji < 30; ji++)
+                                    {
+                                        table[rec_count].name[ji] = buf[ji];
+                                        buf[ji] = '\0';
+                                    }
+                                    break;
+                                case PAY:
+                                    table[rec_count].pay = atof(buf);
+                                    clear_buf(buf, 30);
+                                    break;
+                                case COUNT:
+                                    table[rec_count].count = atoi(buf);
+                                    clear_buf(buf, 30);
+                                    rec_count++;
+                                    i = -1;
+                                    break;
+                                };
+                                i++;
+                                j = 0;
+                                continue;
+                            }
+                            buf[j++] = c;
+                        }
+                        fclose(fp);
+                        for (int i = 0; i < rec_count; i++)
+                            {
+                            for (int ji = 0; ji < 30; ji++)
+                                result[i][0][ji] = table[i].name[ji];
+                            sprintf(result[i][1], "%.2f", table[i].pay / table[i].count);
+                            
+                            }
+                        res_out = 1;
+                    }
+                    else MessageBox(NULL, L"Error while reading file!", L"Error", 0);
+                }
+            }
+    }
+        nk_end(ctx);
+
+        /* -------------- EXAMPLES ---------------- */
+        #ifdef INCLUDE_CALCULATOR
+          calculator(ctx);
+        #endif
+        #ifdef INCLUDE_CANVAS
+          canvas(ctx);
+        #endif
+        #ifdef INCLUDE_OVERVIEW
+          overview(ctx);
+        #endif
+        #ifdef INCLUDE_NODE_EDITOR
+          node_editor(ctx);
+        #endif
+        /* ----------------------------------------- */
+
+        /* Draw */
+        ID3D11DeviceContext_ClearRenderTargetView(context, rt_view, &bg.r);
+        ID3D11DeviceContext_OMSetRenderTargets(context, 1, &rt_view, NULL);
+        nk_d3d11_render(context, NK_ANTI_ALIASING_ON);
+        hr = IDXGISwapChain_Present(swap_chain, 1, 0);
+        if (hr == DXGI_ERROR_DEVICE_RESET || hr == DXGI_ERROR_DEVICE_REMOVED) {
+            /* to recover from this, you'll need to recreate device and all the resources */
+            MessageBoxW(NULL, L"D3D11 device is lost or removed!", L"Error", 0);
+            break;
+        } else if (hr == DXGI_STATUS_OCCLUDED) {
+            /* window is not visible, so vsync won't work. Let's sleep a bit to reduce CPU usage */
+            Sleep(10);
+        }
+        assert(SUCCEEDED(hr));
+    }
+
+    ID3D11DeviceContext_ClearState(context);
+    nk_d3d11_shutdown();
+    ID3D11RenderTargetView_Release(rt_view);
+    ID3D11DeviceContext_Release(context);
+    ID3D11Device_Release(device);
+    IDXGISwapChain_Release(swap_chain);
+    UnregisterClassW(wc.lpszClassName, wc.hInstance);
+    return 0;
+}
